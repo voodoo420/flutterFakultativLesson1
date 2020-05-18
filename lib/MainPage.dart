f7b260a30bea96cbb59dad3cf41bcdbd44603b7d
voodoo420 <voodoo420@mail.ru>
2020-05-18 16:08:41 +0300

    homework 2

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,7 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/main.dart';
+
+import 'ChatPage.dart';
 
 List<Map<String, dynamic>> dataChats = [];
 
@@ -11,9 +12,11 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  TextEditingController _textFieldController = TextEditingController();
+
   @override
   void initState() {
-    firestore.collection("chats").limit(100).snapshots().listen((event) {
+    firestore.collection("chats").orderBy("date").snapshots().listen((event) {
       List<Map<String, dynamic>> data = [];
 
       event.documents.forEach((documentSnapshot) {
@@ -25,10 +28,6 @@ class _MainPageState extends State<MainPage> {
       });
     });
     super.initState();
-  }
-
-  createNewChat(Map<String, dynamic> newChat){
-    firestore.collection("chats").add(newChat);
   }
 
   @override
@@ -45,22 +44,105 @@ class _MainPageState extends State<MainPage> {
         child: dataChats.isEmpty
             ? Text("loading")
             : ListView.builder(
-          itemCount: dataChats.length,
-          itemBuilder: (BuildContext context, int index) {
-            return buildInkWell(context, message: dataChats[index]["message"].toString(),
-                title: dataChats[index]["title"].toString() , isPrivate: dataChats[index]["private"]
-            );
+                itemCount: dataChats.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return buildInkWell(context,
+                      message: dataChats[index]["message"].toString(),
+                      title: dataChats[index]["title"].toString(),
+                      isPrivate: dataChats[index]["private"],
+                      id: dataChats[index]["id"]);
                 },
               ),
       ),
+      floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("chat name"),
+                    content: TextFormField(
+                      controller: _textFieldController,
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: Text("cancel"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text("create"),
+                        onPressed: () {
+                          if (_textFieldController.text != "") {
+                            createChat(_textFieldController.text);
+                            _textFieldController.clear();
+                            Navigator.pop(context);
+                          }
+                        },
+                      )
+                    ],
+                  );
+                });
+          }),
     );
+  }
+
+  void createChat(String title) {
+    Map<String, dynamic> emptyChat = {
+      "message": "",
+      "title": "",
+      "private": false,
+      "date": ""
+    };
+    firestore.collection("chats").add(emptyChat).then((data) {
+      String id = data.documentID;
+      emptyChat = {
+        "message": "empty",
+        "title": title,
+        "private": false,
+        "id": id,
+        "date": DateTime.now().millisecondsSinceEpoch
+      };
+      data.setData(emptyChat);
+    });
   }
 
   Widget buildInkWell(BuildContext context,
       {String message = "Последнее сообщение",
       String title = "",
-      bool isPrivate = false}) {
+      bool isPrivate = false,
+      String id = ""}) {
     return InkWell(
+      onTap: () {
+        _openChat(context, id);
+      },
+      onLongPress: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("delete chat?"),
+                content: Text(title),
+                actions: [
+                  FlatButton(
+                    child: Text("delete"),
+                    onPressed: () {
+                      firestore.collection("chats").document(id).delete();
+                      Navigator.pop(context);
+                    },
+                  ),
+                  FlatButton(
+                    child: Text("no"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              );
+            });
+      },
       child: Container(
         margin: EdgeInsets.all(9.0),
         padding: EdgeInsets.all(9.0),
@@ -81,6 +163,15 @@ class _MainPageState extends State<MainPage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  void _openChat(BuildContext context, String id) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ChatPage(id: id),
       ),
     );
   }
