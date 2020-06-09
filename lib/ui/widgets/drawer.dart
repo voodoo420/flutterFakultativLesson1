97fba88008963db1ac6932aa30c8680173a1d568
voodoo420 <voodoo420@mail.ru>
2020-06-09 18:48:19 +0300

    home work 6

--- lib/ui/widgets/drawer.dart
+++ lib/ui/widgets/drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/themes/custom_theme.dart';
 import 'package:flutterapp/themes/themes.dart';
+import 'package:flutterapp/ui/pages/UserPage.dart';
 
 class MainDrawer extends StatelessWidget {
   void _changeTheme(BuildContext context, {MyThemeKeys key}) {
@@ -14,13 +15,27 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column (children: <Widget>[
-        ListTile (title: Text( "Поменять тему"),
-          onTap: () {
-            _changeTheme(context);
-          },
-        )
-      ],),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 30),
+          ListTile(
+            title: Text("Поменять тему"),
+            onTap: () {
+              _changeTheme(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text("Профиль"),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) {
+                return UserPage();
+              }));
+            },
+          )
+        ],
+      ),
     );
   }
 }
