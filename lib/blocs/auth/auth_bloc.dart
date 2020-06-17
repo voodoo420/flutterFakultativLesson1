069a7be3d4c20cc39bb030378038a7e2977b9c63
voodoo <voodoo420@mail.ru>
2020-06-17 16:50:43 +0300

    verifyNumber() added

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -1,22 +1,48 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterapp/main.dart';
 import './bloc.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
+
+  String verificationId;
 
   @override
   AuthState get initialState => InitialAuthState();
 
   @override
-  Stream<AuthState> mapEventToState(
-    AuthEvent event,
-  ) async* {
-    if(event is AuthNumberEntered){
+  Stream<AuthState> mapEventToState(AuthEvent event,) async* {
+    if (event is AuthNumberEntered) {
       yield* _mapAuthSAuthNumberEnteredToState(event.number);
     }
   }
 
-  Stream<AuthState>_mapAuthSAuthNumberEnteredToState(String number) async* {
-    yield
+  Stream<AuthState> _mapAuthSAuthNumberEnteredToState(String number) async* {
+    await verifyNumber(number);
+  }
+
+  Future<void> verifyNumber(String phoneNumber) async {
+    final PhoneVerificationCompleted verified = (AuthCredential authResult){
+      auth.signInWithCredential(authResult);
+    };
+    final PhoneVerificationFailed failed = (AuthException authException){
+      print(authException.message);
+    };
+
+    final PhoneCodeSent codeSent = (String verificationId, [int forceResend]){
+      this.verificationId = verificationId;
+    };
+
+    final PhoneCodeAutoRetrievalTimeout autoRetrievalTimeout = (String verificationId){
+      this.verificationId = verificationId;
+    };
+
+    await auth.verifyPhoneNumber(phoneNumber: phoneNumber,
+        timeout: const Duration(seconds: 5),
+        verificationCompleted: verified,
+        verificationFailed: failed,
+        codeSent: codeSent,
+        codeAutoRetrievalTimeout: autoRetrievalTimeout);
   }
 }
